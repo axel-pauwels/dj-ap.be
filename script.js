ee6fdd26e6d8d69222c8cc76d19e0c2a6dd298fe
Axel Pauwels <104796446+axel-pauwels@users.noreply.github.com>
2024-11-14 00:26:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";   // Verberg de modal
 }
+
+
+const images = [
+    "images/DJAPDVJ-6.jpg",
+    "images/DJAPDVJ-15.jpg",
+    "images/DJAPDVJ-14.jpg",
+    "images/DJAPDVJ-6.jpg",
+    "images/IMG_0156-08.jpg",
+    "images/IMG-20240702-WA0017.jpg",
+    "images/IMG-20240702-WA0014.jpg"
+];
+
+// Selecteer het element waar de afbeelding wordt getoond
+const slideshowImage = document.getElementById("slideshow-image");
+let currentIndex = 0;
+
+// Functie om naar de volgende afbeelding te gaan
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length; // Zorg dat het opnieuw begint na de laatste afbeelding
+    slideshowImage.src = images[currentIndex];
+}
+
+// Wissel de afbeelding om de 5 seconden
+setInterval(nextImage, 1000);
