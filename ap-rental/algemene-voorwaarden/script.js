dbba5a1668db3c97dbc2d49e68a3934e6d017a43
Axel Pauwels <104796446+axel-pauwels@users.noreply.github.com>
2024-11-19 19:29:41 +0100

    Add files via upload

--- ap-rental/algemene-voorwaarden/script.js
+++ ap-rental/algemene-voorwaarden/script.js
@@ -20,3 +20,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+// gemeenschappelijke FOOTER
+fetch('../../footer.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('footer').innerHTML = data;
+})
+.catch(error => console.error('Error loading footer:', error));
