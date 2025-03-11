c153059affac71c045b4a165e20364e06d169c58
Axel Pauwels <104796446+axel-pauwels@users.noreply.github.com>
2025-03-11 13:42:17 +0100

    Delete 404 directory

--- 404/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector(".button");
-    button.addEventListener("click", (e) => {
-        e.preventDefault();
-        window.location.href = e.target.getAttribute("href");
-    });
-});
